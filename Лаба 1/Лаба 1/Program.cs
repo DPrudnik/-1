d927cb04e1ddff361bb488c4421c15d38a3aeada
DPrudnik <163072566+DPrudnik@users.noreply.github.com>
2024-03-25 01:51:15 +0200

    Add files via upload

---
+++ Лаба 1/Лаба 1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        bool allUnique = AreAllElementsUnique(numbers);
+
+        if (allUnique)
+        {
+            Console.WriteLine("Всі елементи у списку є унікальними.");
+        }
+        else
+        {
+            Console.WriteLine("У списку є неунікальні елементи.");
+        }
+    }
+
+    static bool AreAllElementsUnique(List<int> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            for (int j = i + 1; j < list.Count; j++)
+            {
+                if (list[i] == list[j])
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
